fbbba40862f792a5331cbfd5478dd83a800f07b1
Grimy <45356666+BettySwolax@users.noreply.github.com>
2019-08-30 10:28:21 +0100

    Added interact event to excute skills

--- src/main/java/gg/loski/skills/abilities/AbilitiesHandler.java
+++ src/main/java/gg/loski/skills/abilities/AbilitiesHandler.java
@@ -1,7 +1,15 @@
 package gg.loski.skills.abilities;
 
+import gg.loski.skills.Skills;
 import gg.loski.skills.abilities.type.JellyLegsAbility;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.metadata.MetadataValue;
+import sun.management.counter.perf.PerfLongArrayCounter;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,6 +20,25 @@ public class AbilitiesHandler implements Listener {
 
     public AbilitiesHandler() {
         skills.put("JellyLegs", new JellyLegsAbility());
+
+        Bukkit.getPluginManager().registerEvents(this, Skills.getInstance());
+    }
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent e) {
+        Player player = e.getPlayer();
+
+        if (!e.getAction().name().contains("RIGHT")) return;
+        if (!player.hasMetadata("abilities")) return;
+
+        MetadataValue metadataValue = player.getMetadata("abilities").get(0);
+
+        Abilities abilities = getFromName(metadataValue.asString());
+
+        if (abilities == null) return;
+
+        abilities.excute(e);
+        player.sendMessage(ChatColor.YELLOW + "You have used " + abilities.getName().toUpperCase());
     }
 
     public Abilities getFromName(String name) {
